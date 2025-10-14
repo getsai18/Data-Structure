c32c7fe152e4e3f78c73b5d79dcc7571ed32fae5
getsai18 <20243ds167@utez.edu.mx>
2025-10-13 19:41:45 -0600

    Insertar marcadores (Arr Bi)

--- sumaArregloPar/src/App.java
+++ sumaArregloPar/src/App.java
@@ -9,49 +9,54 @@ public class App {
 
     void ejercicio() {
         int[][] arr = {
+
                 { 1 }, // impar -> agrega 0
                 { 6, 3, 1 }, // impar -> agrega 0
-                { 1, 2, 3 }, // par -> agrega (1+4)=5
+                { 1, 2, 3, 4 }, // par -> agrega (1+4)=5
                 { 0, 5, 3, 0 }, // par -> agrega (0+0)=0
                 { 2, 5, 9 }, // impar -> agrega 0
-                { 2, 4, 8, 9, 10, 1 } // impar -> agrega 0
+                { 2, 4, 8, 9, 10 } // impar -> agrega 0
         };
 
         int[][] nuevoArr = new int[6][];
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i].length % 2 == 0) {
-                    nuevoArr[i] = new int[(arr[i].length) + 1];
 
-                } else {
-                    nuevoArr[i] = new int[arr[i].length + 1];
-                    nuevoArr[i][nuevoArr[i].length - 1] = 0;
-                }
-            }
+        for(int i = 0; i < arr.length; i++){
+            nuevoArr[i] = new int[ (arr[i].length) + 1];
         }
 
-        for (int i = 0; i < nuevoArr.length; i++) {
+        for(int i = 0; i < nuevoArr.length; i++){
+            for(int j = 0; j < nuevoArr[i].length; j ++){
 
-            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i].length % 2 == 0){
 
-                nuevoArr[i][j] = arr[i][j];
+                    if( arr[i].length == j){
+                        nuevoArr[i][j]  = arr[i][0] + arr[i][ ( arr[i].length - 1) ];
+                    }else{
+                        nuevoArr[i][j] = arr[i][j];
+                    }
 
-                if (arr[i].length %2 == 0) {
-                    if (nuevoArr[i].length - 1 == j + 1) {
-                        nuevoArr[i][nuevoArr[i].length - 1] = arr[i][j] + arr[i][0];
+                }else{
+                    
+                    if(arr[i].length == j){
+                        nuevoArr[i][j] = 0;
+                    }else{
+                        nuevoArr[i][j] = arr[i][j];
                     }
                 }
-
             }
-
         }
 
-        for (int[] fila : nuevoArr) {
-            for (int valor : fila) {
-                System.out.print(valor + " ");
+
+        for(int i = 0; i < nuevoArr.length; i++){
+            System.out.print("{");
+            for(int  j = 0; j < nuevoArr[i].length; j++){
+                System.out.print(nuevoArr[i][j]+" ");
             }
-            System.out.println();
+            System.out.print("}\n");
         }
+
+
+
     }
 }
